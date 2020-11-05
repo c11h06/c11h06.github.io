eafa2d31ec1cb1fbd9814cf6a34a725de69268e7
james <james@jamess-mbp.gateway.sonic.net>
2020-11-05 13:03:06 -0800

    installed styling libraries

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Home } from './pages/Home';
 import { Contact } from './pages/Contact';
 import { Portfolio } from './pages/Portfolio';
 import { Layout } from './components/Layout';
-import { NavigationBar } from './components/NavBar';
+import { NavigationBar, NavBar } from './components/NavBar';
 import { Jumbotron } from './components/Jumbotron';
 
 class App extends Component {
