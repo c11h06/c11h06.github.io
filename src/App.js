d763d6e92db11fc7bcf3fd29f122f1588599b96c
james <james@jamess-mbp.gateway.sonic.net>
2020-11-07 11:34:00 -0800

    changed browserrouter to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { Contact } from './pages/Contact';
 import { Portfolio } from './pages/Portfolio';
