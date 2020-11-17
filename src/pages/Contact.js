d2749af359cc549fe3d70ee53d1ebaa21d6dd69d
james <james@jamess-mbp.gateway.sonic.net>
2020-11-17 14:11:21 -0800

    added linkedin and resume links to homepage

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,5 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {Alert} from 'react-bootstrap';
 import emailjs from 'emailjs-com';
+
+const onSuccessfulSend = () => {
+  return (
+    <Alert variant='info' dismissible>
+      <p>Thanks for reaching out! I'll be in touch!</p>
+    </Alert>
+  )
+}
 
 export const Contact = () => {
 
@@ -15,10 +24,11 @@ export const Contact = () => {
         console.log(error.text);
       });
     e.target.reset();
+    onSuccessfulSend();
   }
 
   return (
-    <div>
+    <div class='col text-center'>
       <p>Interested in collaborating? Email me!</p>
       <div className='container'>
         <form onSubmit={sendEmail}>
@@ -26,14 +36,14 @@ export const Contact = () => {
             <div className='col-8 form-group mx-auto'>
               <input type='text' className='form-control' placeholder="Name" name='name' />
             </div>
-            <div className='col-8 form-group pt-2 mx-auto'>
+            <div className='col-8 form-group mx-auto'>
               <input type='text' className='form-control' placeholder="Subject" name='subject' />
             </div>
             <div className='col-8 form-group mx-auto'>
               <textarea className='form-control' id='' cols='30' rows='8' placeholder='Your message' name='message'></textarea>
             </div><br></br>
           </div>
-        <div classname='col-8 pt-3 mx-auto'>
+        <div className='col-8 pt-3 mx-auto'>
           <input type='submit' className='btn btn-info' value='Send Message'></input>
         </div>
         </form>
